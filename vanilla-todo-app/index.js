2ca5e84bba582411c5ec35347c187655d1c2d5d8
Gal <galc@wix.com>
2021-03-25 11:28:05 +0200

    added add todo logic

--- vanilla-todo-app/index.js
+++ vanilla-todo-app/index.js
@@ -1,1 +1,47 @@
+const elementClasses = {
+    todoList:'.todo-list',
+}
+
+const elementSelectors = {
+    todoList : ()=>{return document.querySelector(elementClasses.todoList)},
+}
+
+class TODOManager{
+    constructor(){
+        this.todos = [];
+        this._initEventListeners();
+    }
+
+
+    _getTODOTemplate({content, id}){
+        return `<div class="todo-item" id="${id}">
+        <p>${content}</p>
+    </div>`
+    }
+
+    _renderTodo({content, id}){
+        const todoHTMLList = elementSelectors.todoList();
+        todoHTMLList.insertAdjacentHTML('beforeend', this._getTODOTemplate({content, id}))
+    }
+
+    _handleAddTodoClick(){
+        
+    }
+
+    _initEventListeners(){
+
+    }
+
+    addTodo(content){
+        const id = this.todos.length;
+        const todo = {content, id};
+        this.todos.push(todo);
+        this._renderTodo(todo);
+    }
+}
+
+const todoManager = new TODOManager();
+
 console.log('test');
+todoManager.addTodo('blablabla');
+todoManager.addTodo('bsdfdsffsda');
