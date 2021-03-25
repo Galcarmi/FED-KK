20a94ced457b85f7ca2b0debfcb13eff6d6e2572
Gal <galc@wix.com>
2021-03-25 13:20:54 +0200

    added done functionality

--- vanilla-todo-app/index.js
+++ vanilla-todo-app/index.js
@@ -5,6 +5,7 @@ const elementClasses = {
     actionDoneSVG: '.action-done-svg',
     actionDeleteSVG: '.action-delete-svg',
     actionEditSVG: '.action-edit-svg',
+    todoItemContent:'.todo-item-content',
 }
 
 const elementSelectors = {
@@ -23,7 +24,7 @@ class TODOManager{
 
     _getTODOTemplate({content, id}){
         return `<div class="todo-item" id="${id}">
-        <p>${content}</p>
+        <p class="todo-item-content">${content}</p>
         <div class="todo-item-actions">
                         <svg class ="action-done-svg"xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.393 7.5l-5.643 5.784-2.644-2.506-1.856 1.858 4.5 4.364 7.5-7.643-1.857-1.857z"/></svg>
                         <svg class = "action-delete-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.538l-4.592-4.548 4.546-4.587-1.416-1.403-4.545 4.589-4.588-4.543-1.405 1.405 4.593 4.552-4.547 4.592 1.405 1.405 4.555-4.596 4.591 4.55 1.403-1.416z"/></svg>
@@ -51,8 +52,13 @@ class TODOManager{
 
     _addEventListenersForTODOItem({id}){
         const todoItem = elementSelectors.getTODOItemById(id);
-        todoItem.querySelector(elementClasses.actionDeleteSVG).addEventListener('click',()=>{console.log('delete')});
-        todoItem.querySelector(elementClasses.actionDoneSVG).addEventListener('click',()=>{console.log('done')})
+        todoItem.querySelector(elementClasses.actionDeleteSVG).addEventListener('click',()=>{
+            console.log('delete');
+        });
+        todoItem.querySelector(elementClasses.actionDoneSVG).addEventListener('click',()=>{
+            console.log('done', todoItem.querySelector(elementClasses.todoItemContent))
+            todoItem.querySelector(elementClasses.todoItemContent).classList.toggle('crossed-content')
+        })
         todoItem.querySelector(elementClasses.actionEditSVG).addEventListener('click',()=>{console.log('edit')})
     }
 
