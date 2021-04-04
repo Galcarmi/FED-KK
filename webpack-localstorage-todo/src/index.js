6d129b31c117ae213c226e0dffb45e8c20ebb46b
Gal <galc@wix.com>
2021-04-04 12:59:48 +0300

    added basic mvc structure

--- webpack-localstorage-todo/src/index.js
+++ webpack-localstorage-todo/src/index.js
@@ -1,3 +1,10 @@
-import './styles.css';
+import "./styles.css";
+import { View } from "./js/View";
+import { Model } from "./js/Model";
+import { Controller } from "./js/Controller";
 
-console.log('lalala');
+const app = new Controller(new Model(), new View())
+app.view.addTodo({content:'asdasd', id:"2"})
+app.view.addTodo({content:'czv', id:"3"})
+app.view.addTodo({content:'asdasd', id:"4"})
+console.log("lalala");
---
+++ webpack-localstorage-todo/src/js/constants.js
@@ -0,0 +1,23 @@
+export const elementClasses = {
+    todoList:'.todo-app__list',
+    actionTODOBtn: '.todo-app__input-container__add-btn',
+    todoTxtInput:'.todo-app__input-container__text-input',
+    actionDoneSVG: '.todo-app__list__item__actions__done',
+    actionDeleteSVG: '.todo-app__list__item__actions__delete',
+    actionEditSVG: '.todo-app__list__item__actions__edit',
+    todoItemContent:'.todo-app__list__item__content',
+    todoEmptyState: '.todo-empty-state',
+}
+
+export const elementSelectors = {
+    todoList : ()=>document.querySelector(elementClasses.todoList),
+    actionTODOBtn : ()=> document.querySelector(elementClasses.actionTODOBtn),
+    todoTxtInput : ()=> document.querySelector(elementClasses.todoTxtInput),
+    getTODOItemById : (id) => document.querySelector(`[id='${id}']`),
+    todoEmptyState : () => document.querySelector(elementClasses.todoEmptyState),
+}
+
+export const eTODOActionBtnMode = {
+    EDIT:'EDIT',
+    ADD:'ADD',
+}
