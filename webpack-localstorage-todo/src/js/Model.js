67c100d68ea61aaa1865a658f4b3b2d5dc717318
Gal <galc@wix.com>
2021-04-04 13:27:40 +0300

    added model features

--- webpack-localstorage-todo/src/js/Model.js
+++ webpack-localstorage-todo/src/js/Model.js
@@ -1,3 +1,19 @@
 export class Model{
-    constructor(){}
+    constructor(){
+        this.todos = [];
+    }
+
+    addTodo(todo){
+        this.todos.push(todo);
+    }
+
+    deleteTodoById(id){
+        const deletedIndex = this.todos.findIndex(todo=>todo.id === id);
+        this.todos.splice(deletedIndex,1);
+    }
+
+    editTodoContentById({id, content}){
+        const editedIndex = this.todos.findIndex(todo=>todo.id === id);
+        this.todos[editedIndex].content = content;
+    }
 }
