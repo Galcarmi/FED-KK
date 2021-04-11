084f039746be20f64f5b82082a11ca9f4e63ce6f
Gal <galc@wix.com>
2021-04-11 15:43:12 +0300

    added error log in error utils

--- express-server/errors/utils.js
+++ express-server/errors/utils.js
@@ -1,6 +1,7 @@
 import { ServerError } from './ServerError.js';
 
 export const handleServerError = (e, res)=>{
+    console.error(e);
     if(e instanceof ServerError){
         res.status(e.HTTPStatus).send(e.message);
     }
