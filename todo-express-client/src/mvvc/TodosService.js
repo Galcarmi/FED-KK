1231e74307f199d2523920c151ad7708612cb19d
Gal <galc@wix.com>
2021-04-13 13:01:04 +0300

    fixed server url

--- todo-express-client/src/mvvc/TodosService.js
+++ todo-express-client/src/mvvc/TodosService.js
@@ -1,25 +1,27 @@
 import axios from 'axios';
 
+const serverUrl = 'http://localhost:8000';
+
 class TodosService{
     constructor(){}
 
     async addTodo(todo){
-        const res = await axios.post('/todo', todo);
+        const res = await axios.post(`${serverUrl}/todo`, todo);
         return res.data;
     }
 
     async deleteTodo(id){
-        const res = await axios.delete('/todo', {id});
+        const res = await axios.delete(`${serverUrl}/todo`, {id});
         return res.data;
     }
 
     async editTodo(todo){
-        const res = await axios.put('/todo', todo);
+        const res = await axios.put(`${serverUrl}/todo`, todo);
         return res.data;
     }
 
     async getAllTodos(){
-        const res = await axios.get('/todos');
+        const res = await axios.get(`${serverUrl}/todos`);
         return res.data;
     }
 }
