091df20349c66ae96b6e3bcba19be0638544e7e4
Gal <galc@wix.com>
2021-04-18 15:05:11 +0300

    refactored error handling

--- express-server/index.js
+++ express-server/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { TodoInMemoryDBManager } = require('./DBManager/TodoInMemoryDBManager.js');
-const { handleServerError } = require('./errors/utils.js');
 const { logger } = require('./logger/logger.js');
 const { eClientLocations } = require('./constants/clientLocations.js');
+const { errorMiddleware, wrapError } = require('./middleware/errorHandler');
 
 const parentFolder = path.join(__dirname, '../')
 const todoInMemoryDBManager = new TodoInMemoryDBManager();
@@ -15,47 +14,33 @@ const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use(express.static(parentFolder + eClientLocations.TASK_4_CLIENT_DIST));
 
-app.get('/', (req, res) => {
+app.get('/', wrapError((req, res) => {
   res.sendFile(`${eClientLocations.TASK_4_CLIENT_DIST}/index.html`, {
     root: parentFolder,
   });
-});
+}));
 
-app.post('/todo', (req, res) => {
-  try {
+app.post('/todo', wrapError((req, res) => {
     const addedTodo = todoInMemoryDBManager.addTodo(req.body);
     res.status(200).send(addedTodo);
-  } catch (e) {
-    handleServerError(e, res);
-  }
-});
+}));
 
-app.put('/todo', (req, res) => {
-  try {
+app.put('/todo', wrapError((req, res) => {
     const editedTodo = todoInMemoryDBManager.editTodo(req.body);
     res.status(200).send(editedTodo);
-  } catch (e) {
-    handleServerError(e, res);
-  }
-});
+}));
 
-app.delete('/todo/:id', (req, res) => {
-  try {
+app.delete('/todo/:id', wrapError((req, res) => {
     const deletedTodo = todoInMemoryDBManager.removeTodo(req.params.id);
-    res.status(200).send(deletedTodo);
-  } catch (e) {
-    handleServerError(e, res);
-  }
-});
+    res.status(200).send(deletedTodo);  
+}));
 
-app.get('/todos', (req, res) => {
-  try {
+app.get('/todos', wrapError((req, res) => {
     const todos = todoInMemoryDBManager.getAllTodos();
-    res.status(200).send(todos);
-  } catch (e) {
-    handleServerError(e, res);
-  }
-});
+    res.status(200).send(todos);  
+}));
+
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   logger.info(`listening on ${port}`);
---
+++ express-server/middleware/userIdMiddleware.js
@@ -0,0 +1,3 @@
+const userIdMiddleware = (req, res, next) => {
+  
+};
