3e04969ac9f8f9a13e2a4968db18376a3a5ac9fc
Gal <galc@wix.com>
2021-05-02 14:03:06 +0300

    added common styles

---
+++ packages/5-todo-typescript/src/styles/commonStyles.ts
@@ -0,0 +1,70 @@
+export const colors: { [key: string]: string } = {
+  softBlue: 'rgba(82, 153, 229, 1)',
+  blue: 'rgb(0, 123, 255)',
+  softYellow: 'rgb(177, 177, 41)',
+  white: 'white',
+  red: 'red',
+  green: 'green',
+};
+
+export const commonStyles: { [key: string]: { [key: string]: string } } = {
+  sidesMarginBigPercent: {
+    marginLeft: '10%',
+    marginRight: '10%',
+  },
+  sidesMarginMediumPercent: {
+    marginLeft: '5%',
+    marginRight: '5%',
+  },
+  sidesMarginSmallPercent: {
+    marginLeft: '2%',
+    marginRight: '2%',
+  },
+  sidesPaddingS: {
+    paddingLeft: '10px',
+    paddingRight: '10px',
+  },
+  paddingTopBottomS: {
+    paddingTop: '5px',
+    paddingBottom: '5px',
+  },
+  marginTopBottomMedium: {
+    marginTop: '7px',
+    marginBottom: '7px',
+  },
+  sidesMarginXS: {
+    marginLeft: '3px',
+    marginRight: '3px',
+  },
+  sidesMarginS: {
+    marginLeft: '5px',
+    marginRight: '5px',
+  },
+  marginRightS: {
+    marginRight: '5px',
+  },
+  smallRoundedBorder: {
+    borderRadius: '25px',
+  },
+  fontSmall: {
+    fontSize: '1.2em',
+  },
+  fontMedium: {
+    fontSize: '1.4em',
+  },
+  minTodoItemHeight: {
+    minHeight: '35px',
+  },
+  mediumSizeShadow: {
+    boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+  },
+  greyTopBorder: {
+    borderTop: 'solid 3px rgb(177, 184, 189)',
+  },
+  blackBorder: {
+    border: 'solid 2px rgb(62, 66, 70)',
+  },
+  focusedBlueBorder: {
+    border: 'solid 3px rgba(82, 153, 229, 1)',
+  },
+};
