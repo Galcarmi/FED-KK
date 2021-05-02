bc3278fc13837419b0afba67753b3bc70d5238f1
Gal <galc@wix.com>
2021-05-02 16:11:31 +0300

    added types to axios requests

--- packages/5-todo-typescript/src/services/TodosService.ts
+++ packages/5-todo-typescript/src/services/TodosService.ts
@@ -4,22 +4,22 @@ class TodosService {
   constructor() {}
 
   async addTodo(todo: ITodoDTO): Promise<ITodoDTO> {
-    const res = await axios.post(`/todo`, todo);
+    const res = await axios.post<ITodoDTO>(`/todo`, todo);
     return res.data;
   }
 
   async deleteTodo(_id: string): Promise<ITodoDTO> {
-    const res = await axios.delete(`/todo/${_id}`);
+    const res = await axios.delete<ITodoDTO>(`/todo/${_id}`);
     return res.data;
   }
 
   async editTodo(todo: Partial<ITodoDTO>): Promise<ITodoDTO> {
-    const res = await axios.put(`/todo`, todo);
+    const res = await axios.put<ITodoDTO>(`/todo`, todo);
     return res.data;
   }
 
   async getAllTodos(): Promise<ITodoDTO[]> {
-    const res = await axios.get(`/todos`);
+    const res = await axios.get<ITodoDTO[]>(`/todos`);
     return res.data;
   }
 }
