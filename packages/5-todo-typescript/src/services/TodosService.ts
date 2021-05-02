3aa2184a13a306361a4ac3c52c7d37da41f5c996
Gal <galc@wix.com>
2021-05-02 16:07:29 +0300

    fixed todos service & used common module

--- packages/5-todo-typescript/src/services/TodosService.ts
+++ packages/5-todo-typescript/src/services/TodosService.ts
@@ -1,24 +1,24 @@
 import axios from 'axios';
-
+import { ITodoDTO } from 'fed-todo-journey_todo-common';
 class TodosService {
   constructor() {}
 
-  async addTodo(todo) {
+  async addTodo(todo: ITodoDTO): Promise<ITodoDTO> {
     const res = await axios.post(`/todo`, todo);
     return res.data;
   }
 
-  async deleteTodo(_id) {
+  async deleteTodo(_id: string): Promise<ITodoDTO> {
     const res = await axios.delete(`/todo/${_id}`);
     return res.data;
   }
 
-  async editTodo(todo) {
+  async editTodo(todo: Partial<ITodoDTO>): Promise<ITodoDTO> {
     const res = await axios.put(`/todo`, todo);
     return res.data;
   }
 
-  async getAllTodos() {
+  async getAllTodos(): Promise<ITodoDTO[]> {
     const res = await axios.get(`/todos`);
     return res.data;
   }
