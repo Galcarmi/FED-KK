67c2bd54e8d7038d048ea9fd63a0184477aa323a
Gal <galc@wix.com>
2021-05-05 20:06:18 +0300

    exported istestenv directly from utils

--- packages/todo-common/index.ts
+++ packages/todo-common/index.ts
@@ -2,4 +2,4 @@ export { ITodoDTO } from './dto/todo/ITodoDTO';
 export { ITodoIdentifier } from './dto/todo/ITodoIdentifier';
 export { Nullable } from './dto/utils/NullableDTO';
 export { PartialBy } from './types/common-types/PartialBy';
-export { utils } from './utils/utils';
+export * as utils from './utils/utils';
