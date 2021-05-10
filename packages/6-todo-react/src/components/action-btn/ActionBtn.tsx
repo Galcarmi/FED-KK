29f20165027abf752ea47261183f9f1eacf430e6
Gal <galc@wix.com>
2021-05-10 11:12:03 +0300

    merged svgs to the same component

---
+++ packages/6-todo-react/src/components/action-btn/ActionBtn.tsx
@@ -0,0 +1,41 @@
+import React, { FC, MouseEventHandler, ReactElement } from 'react';
+import { commonStyles } from '../../styles/commonStyles';
+import { jss } from '../../styles/jss';
+
+interface BtnProps {
+  btnHandler: MouseEventHandler<SVGSVGElement>;
+  color: string;
+  svgIconPath: JSX.Element;
+}
+
+
+export const ActionBtn: FC<BtnProps> = (props: BtnProps): ReactElement => {
+
+  const getStyles = (color:string)=>{
+    return jss
+    .createStyleSheet({
+      actionBtn: {
+        ...commonStyles.RLMarginXS,
+        cursor: 'pointer',
+        fill: color,
+      },
+    })
+    .attach().classes;
+  }
+
+  const s = getStyles(props.color);
+
+  return (<svg
+    onClick={props.btnHandler}
+    className={s.actionBtn}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24">
+    {props.svgIconPath}
+  </svg>);
+};
+
+
+
+
