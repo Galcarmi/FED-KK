97a97d9d52867c90b9a93d5e3384cb64a8574657
Gal <galc@wix.com>
2021-05-12 14:03:26 +0300

    removed redundant fragment component

--- packages/7-todo-tdd/src/components/root-components/App.tsx
+++ packages/7-todo-tdd/src/components/root-components/App.tsx
@@ -1,15 +1,12 @@
 import React, { FC, ReactElement } from 'react';
-import TodoInput from '../todo-input/TodoInput';
 
 const App: FC = (): ReactElement => {
   return (
-    <>
       <div id='todo'>
         <div id='todo__list'></div>
         <input id='todo__input' type='text' />
         <button id='todo__add-btn'></button>
       </div>
-    </>
   );
 }
 
--- packages/7-todo-tdd/src/components/todo-input/TodoInput.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC, ReactElement } from 'react';
-
-const TodoInput: FC = (): ReactElement => {
-  return (
-    <div>asd</div>
-  );
-}
-
-export default TodoInput;
