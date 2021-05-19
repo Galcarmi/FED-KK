0a5e5557a3e22fff9157c27dd72fe1b6f96b3520
Gal <galc@wix.com>
2021-05-19 18:32:38 +0300

    added container

--- packages/7-todo-tdd/src/components/todo-app/TodoApp.tsx
+++ packages/7-todo-tdd/src/components/todo-app/TodoApp.tsx
@@ -4,6 +4,7 @@ import { TodoItem } from '../todo-item/TodoItem';
 import { ITodoContext, TodoContext } from '../../context/TodoContext';
 import withTodosContex from '../HOC/withTodos';
 import { ITodoDTO } from 'fed-todo-journey_todo-common';
+import { commonStyles, colors } from '../../styles/commonStyles';
 
 const TodoApp: FC<{}> = (): ReactElement => {
   const [todoInputValue, setTodoInputValue] = useState<string>('');
@@ -33,27 +34,49 @@ const TodoApp: FC<{}> = (): ReactElement => {
   }, [])
 
   return (
-    <div className={s.todo}>
-      <div className={s.todo__list}>
-        {Object.values(todos).map(todo => <TodoItem key={todo._id} todo={todo} />)}
+    <div className={s.container}>
+      <div className={s.todo}>
+        <div className={s.todo__list}>
+          {Object.values(todos).map(todo => <TodoItem key={todo._id} todo={todo} />)}
+        </div>
+        <input type='text'
+          className={s.todo__input}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => { setTodoInputValue(e.target.value) }}
+          value={todoInputValue}
+          onKeyPress={onTodoInputEnter} />
+        <button className={s.todo__addBtn} onClick={onAddClick}>Add</button>
       </div>
-      <input type='text'
-        className={s.todo__input}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => { setTodoInputValue(e.target.value) }}
-        value={todoInputValue}
-        onKeyPress={onTodoInputEnter} />
-      <button className={s.todo__addBtn} onClick={onAddClick}>Add</button>
     </div>
   );
 }
 
 export const s = jss
   .createStyleSheet({
+    container: {
+      height: '100vh',
+      ...commonStyles.RLMarginLPercent,
+    },
+    '@media only screen and (max-width: 600px)': {
+      container: {
+        ...commonStyles.RLMarginMPercent,
+      },
+    },
+    '@media only screen and (max-width: 400px)': {
+      container: {
+        ...commonStyles.RLMarginSPercent,
+      },
+    },
     todo: {
-
+      ...commonStyles.TpaddingXXS,
+      ...commonStyles.boxShadowM,
+      backgroundColor: colors.white,
+      minHeight: '100%',
+      width: '100%',
+      position: 'relative',
     },
     todo__list: {
-
+      paddingBottom: `${commonStyles.minTodoItemHeight.minHeight.split('px')[0]
+        }px`,
     },
     todo__input: {
 
