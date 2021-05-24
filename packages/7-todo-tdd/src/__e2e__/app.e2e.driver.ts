8b60ee9728c7ba87f17219bcceba7a7fa61d4b40
Gal <galc@wix.com>
2021-05-24 17:59:31 +0300

    added e2e tests

--- packages/7-todo-tdd/src/__e2e__/app.e2e.driver.ts
+++ packages/7-todo-tdd/src/__e2e__/app.e2e.driver.ts
@@ -1,36 +1,114 @@
-import puppeteer from 'puppeteer';
+import { Browser, ElementHandle, HTTPResponse, launch, Page } from 'puppeteer';
 
 export class AppDriver {
-  public page!: puppeteer.Page;
-  private browser!: puppeteer.Browser;
+  public page!: Page;
+  private browser!: Browser;
 
+  public given = {
+    createTodo: async (todoContent: string): Promise<void> => {
+      await this.sleep(500);
+      await this.insertContentTodoInput(todoContent);
+      await this.clickOnAddTodo();
+      await this.sleep(500);
+    },
+  }
+
+  public when = {
+    clickingOnFirstTodoDoneBtn: async ():Promise<void> => {
+      const todoItem = await this.page.$('[data-hook="TODO_ITEM"]') as ElementHandle<Element>;
+      const doneBtn = await todoItem.$('[data-hook="TODO_ITEM_DONE"]')
+      await doneBtn?.click();
+      await this.sleep(500);
+    },
+    clickingOnFirstTodoDeleteBtn: async ():Promise<void> => {
+      const todoItem = await this.page.$('[data-hook="TODO_ITEM"]') as ElementHandle<Element>;
+      const deleteBtn = await todoItem.$('[data-hook="TODO_ITEM_DELETE"]')
+      await deleteBtn?.click();
+      await this.sleep(500);
+    },
+    editingFirstTodo: async (content:string): Promise<void> => {
+      const todoItem = await this.page.$('[data-hook="TODO_ITEM"]') as ElementHandle<Element>;
+      const editBtn = await todoItem.$('[data-hook="TODO_ITEM_EDIT"]')
+      await editBtn?.click();
+
+      const todoEditInput = await this.page.$('[data-hook="TODO_ITEM_EDIT_INPUT"]') as ElementHandle<Element>;
+      await this.insertContentToInputElem(todoEditInput, content)
+
+      await todoEditInput.evaluate((elem:HTMLInputElement)=>{elem.blur()})
+
+      return this.sleep(500);
+    }
+  }
+
+  public then = {
+    getFirstTodoContent: async (): Promise<string | undefined> => {
+      const todoItems = await this.page.$('[data-hook="TODO_ITEM"]');
+      return todoItems ?.$eval(
+        '[data-hook="TODO_ITEM_CONTENT"]',
+        (elem: Element) => elem.innerHTML
+      );
+    },
+    getTodosCount: async (): Promise<number> => {
+      const todoItems = await this.page.$$('[data-hook="TODO_ITEM"]');
+      
+      return todoItems.length;
+    },
+    isFirstTodoDone: async (): Promise<boolean> => {
+      const todoItem = await this.page.$('[data-hook="TODO_ITEM"]') as ElementHandle<Element>;
+      return todoItem.$eval(
+        '[data-hook="TODO_ITEM_CONTENT"]',
+        (elem: Element) => {
+          let isDone = false;
+          elem.classList.forEach((clazz: string): void => {
+            if (clazz.includes('crossedContent')) {
+              isDone = true;
+            }
+          })
+
+          return isDone;
+        }
+      );
+    }
+  }
+  
   public async launchBrowser() {
-    this.browser = await puppeteer.launch({
-      headless: false,
+    this.browser = await launch({
+      headless: true,
       executablePath:
         '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
     });
     this.page = await this.browser.newPage();
   }
 
-  public async navigateToTodoPage(){
+  public async navigateToTodoPage():Promise<HTTPResponse> {
     return this.page.goto('http://localhost:8000/');
   }
 
-  public async insertContentTodoInput(content: string) {
-    await this.page.focus('[data-hook="TODO_INPUT"]');
-    await this.page.keyboard.type(content);
+  public async insertContentTodoInput(content: string):Promise<void> {
+    const todoInput = await this.page.$('[data-hook="TODO_INPUT"]') as ElementHandle<Element>;
+
+    return this.insertContentToInputElem(todoInput, content);
   }
 
-  public async clickOnAddTodo(){
+  public async clickOnAddTodo():Promise<void> {
     return this.page.click('[data-hook="ADD_BTN"]');
   }
 
-  public async closeBrowser(){
+  public async closeBrowser() :Promise<void>{
     return this.browser.close();
   }
 
-  public async sleep(milli:number){
-      return new Promise((res,rej)=>{setTimeout(res,milli)})
+  public async sleep(milli: number) :Promise<void> {
+    return new Promise((res, rej) => {
+      setTimeout(res, milli);
+    });
+  }
+
+  private async insertContentToInputElem(elem: ElementHandle<Element>, content: string): Promise<void>{
+    await elem.evaluate((elem: HTMLInputElement) => { elem.value = '' });
+    await elem.focus();
+
+    return this.page.keyboard.type(content);
   }
 }
+
--- packages/7-todo-tdd/src/__e2e__/app.e2e.test.ts
+++ packages/7-todo-tdd/src/__e2e__/app.e2e.test.ts
@@ -1,36 +1,80 @@
 import { Chance } from 'chance';
-import { dataHooks } from '../components/utils/dataHooks';
 import { AppDriver } from './app.e2e.driver';
 
 const chance = new Chance();
 
-jest.setTimeout(30000);
-// dataHooks.TODO_ITEM_CONTENT
-
-describe('first test', () => {
+describe('add todos test', () => {
   let appDriver: AppDriver;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     appDriver = new AppDriver();
-  });
-
-  it('should render the todos page', async () => {
     await appDriver.launchBrowser();
     await appDriver.navigateToTodoPage();
+  });
 
+  it('should render todo properly', async ():Promise<void> => {
     const testContent = chance.word();
-    await appDriver.sleep(500);
-    await appDriver.insertContentTodoInput(testContent);
-    await appDriver.clickOnAddTodo();
-    await appDriver.sleep(500);
+    await appDriver.given.createTodo(testContent)
 
-    const todoItems = await appDriver.page.$('[data-hook="TODO_ITEM"]');
-    const todoContent = await todoItems?.$eval(
-      '[data-hook="TODO_ITEM_CONTENT"]',
-      (elem) => elem.innerHTML
-    );
+    const todoContent = await appDriver.then.getFirstTodoContent();
 
     expect(todoContent).toBe(testContent);
+  });
+
+  it('should render isDone state properly', async () :Promise<void>=> {
+    await appDriver.given.createTodo(chance.word());
+
+    const isDone = await appDriver.then.isFirstTodoDone();
+
+    expect(isDone).toBe(false);
+  })
+
+  afterEach(async () => {
+    await appDriver.closeBrowser();
+  });
+});
+
+
+
+describe('todo item action tests', () => {
+  let appDriver: AppDriver;
+
+  beforeEach(async () => {
+    appDriver = new AppDriver();
+    await appDriver.launchBrowser();
+    await appDriver.navigateToTodoPage();
+  });
+
+  it('should mark todo as done', async () => {
+    await appDriver.given.createTodo(chance.word())
+
+    await appDriver.when.clickingOnFirstTodoDoneBtn();
+
+    const isDone = await appDriver.then.isFirstTodoDone();
+
+    expect(isDone).toBe(true);
+  });
+
+  it('should delete first todo', async () => {
+    await appDriver.given.createTodo(chance.word())
+
+    await appDriver.when.clickingOnFirstTodoDeleteBtn();
+
+    const todosCount = await appDriver.then.getTodosCount();
+
+    expect(todosCount).toBe(0);
+  });
+
+  it('should edit first todo', async () => {
+    await appDriver.given.createTodo(chance.word())
+
+    const newTodoContent: string = chance.word();
+
+    await appDriver.when.editingFirstTodo(newTodoContent);
+
+    const todoContent = await appDriver.then.getFirstTodoContent();
+
+    expect(todoContent).toBe(newTodoContent);
   });
 
   afterEach(async () => {
